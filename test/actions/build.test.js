12215522fb577ace10e6da5af04ea2a94550b7e1
eptaccio <eptaccio@gmail.com>
2017-12-13 22:45:36 -0200

    simple refactors

--- test/actions/build.test.js
+++ test/actions/build.test.js
@@ -4,5 +4,5 @@ const logger = require('../../dist/util/logger')
 test('build action', () => {
   logger.error = jest.fn()
   actions.build('dist')
-  expect(logger.error).toBeCalledWith(`error: You need to be inside the folder of your project!\n\nFolder "languages" could not be found in this directory.`)
+  expect(logger.error).toBeCalledWith(`You need to be inside the folder of your project!\n\nFolder "languages" could not be found in this directory.`)
 })
